dc775a92ab73cffa547747431e3d2a523564e940
jotamath <01jmnc@gmail.com>
2024-09-20 17:02:27 -0300

    add: adicionar projeto

---
+++ src/test/java/io/github/jotamath/projetomensageria/ProjetomensageriaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jotamath.projetomensageria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetomensageriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
